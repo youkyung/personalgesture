92f52cf2a34e3f2efa35f7128abad74b45f20193
kimozzi <chkm2089@gmail.com>
2016-01-29 15:52:21 +0900

    urls.py

--- personalgesture/urls.py
+++ personalgesture/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'protest.views.index'),
+    url(r'^index/$', 'protest.views.index'),
+    url(r'^protests/$', 'protest.views.post_list'),
+    url(r'^protests/(?P<pk>\d+)/$', 'protest.views.post_detail'),
+    url(r'^protests/new/$', 'protest.views.protest_new'),
+    url(r'^protests/(?P<pk>\d+)/edit/$', 'protest.views.protest_edit'),
+    # url(r'^protests/(?P<protest_pk>\d+)/comments/new/$', 'protest.views.comment_new'),
+    # url(r'^protests/(?P<protest_pk>\d+)/comments/(?P<pk>\d+)/edit/$', 'protest.views.comment_edit'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
