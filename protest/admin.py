8eaaaf8d640c1441adaa8a1c409ccf9f88a790ad
HeeyoungPark91 <octoberhee0@gmail.com>
2016-01-29 15:20:21 +0900

    admin

--- protest/admin.py
+++ protest/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from blog.models import Protest
+from blog.models import User
+from blog.models import Participation, Donation
 # Register your models here.
+
+
+class ProtestAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_display_links = ['title']
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['nickname', 'email']
+    list_display_links = ['nickname']
+
+class ParticipationAdmin(admin.ModelAdmin):
+    list_display = ['protest', 'user']
+    list_display_links = ['protest']
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ['protest', 'amount', 'account_number']
+    list_display_links = ['protest']
+
+
+admin.site.register(Protest,ProtestAdmin)
+admin.site.register(User,UserAdmin)
+admin.site.register(Participation,ParticipationAdmin)
+admin.site.register(Donation, DonationAdmin)
