9448c138888babb290f9e1bb796b69dd70714fa1
kimozzi <chkm2089@gmail.com>
2016-01-31 18:07:53 +0900

    protest_detail과 user_detail 구현

--- personalgesture/urls.py
+++ personalgesture/urls.py
@@ -22,14 +22,13 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'protest.views.index'),
     url(r'^index/$', 'protest.views.index'),
-    # url(r'^protests/$', 'protest.views.post_list'),
-    url(r'^protests/$', 'protest.views.post_list'),
-    # url(r'^protests/(?P<pk>\d+)/$', 'protest.views.protest_detail'),
-    # url(r'^protests/(?P<pk>\d+)/$', 'protest.views.post_detail'),
+    url(r'^protests/$', 'protest.views.protest_list'),
+    url(r'^protests/(?P<pk>\d+)/$', 'protest.views.protest_detail'),
     url(r'^protests/new/$', 'protest.views.protest_new'),
-    url(r'^protests/(?P<pk>\d+)/edit/$', 'protest.views.protest_edit'),
+    # url(r'^protests/(?P<pk>\d+)/edit/$', 'protest.views.protest_edit'),
     # url(r'^protests/(?P<protest_pk>\d+)/comments/new/$', 'protest.views.comment_new'),
     # url(r'^protests/(?P<protest_pk>\d+)/comments/(?P<pk>\d+)/edit/$', 'protest.views.comment_edit'),
+    url(r'^users/(?P<pk>\d+)/$', 'protest.views.user_detail'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
--- protest/views.py
+++ protest/views.py
@@ -1,18 +1,45 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views.generic import DetailView
 from protest.forms import ProtestForm
-from protest.models import Protest
+from protest.models import Protest, User, Participation
+
 # Create your views here.
 
 def index(request) :
     return render(request, 'protest/index.html')
 
-def post_list(request) :
+def protest_list(request) :
 
-    post_list = Protest.objects.all()
+    protest_list = Protest.objects.all()
     params = {
-    'post_list' : post_list,
+    'protest_list' : protest_list,
     }
-    return render(request, 'protest/post_list.html', params)
+    return render(request, 'protest/protest_list.html', params)
+
+
+# def protest_detail(request):
+#     protest = Protest.objects.get()
+#     return render(request, 'protest/protest_detail.html')
+
+
+def user_detail(request):
+    user = User.objects.get()
+    participation = Participation.objects.get(pk = self.kwargs['pk'])
+    return render(request, 'protest/user_detail.html')
+
+
+class ProtestDetailView(DetailView):
+    def get_object(self, queryset=None):
+        # self.kwargs : year, month, day, pk
+        # self.kwargs['year']
+        # try:
+        #     return Post.objects.get(pk=self.kwargs['pk'])
+        # except Post.DoesNotExist:
+        #     raise Http404
+        return get_object_or_404(Protest, pk=self.kwargs['pk'])
+
+# post_detail = DetailView.as_view(model=Post)
+protest_detail = ProtestDetailView.as_view()
 
 
 def protest_new(request):
@@ -44,3 +71,5 @@ def protest_edit(request, pk):
     return render(request, 'templates/protest_form.html', {
         'form' : form,
         })
+
+user_detail = DetailView.as_view(model = User)
